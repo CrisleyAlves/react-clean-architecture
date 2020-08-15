9369781ee61c318f96afc6b4fa432a10711ec348
Crisley Alves <crisleyalvesphx@gmail.com>
2020-08-15 16:13:32 -0300

    feat: ensure RemoteAuthentication throws correct error

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -2,6 +2,9 @@ import { HttpPostClient } from '@/data/protocols/http/http-post-client'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCrentialsError } from '@/domain/errors/invalid-credentials-error'
+import { UnexpectedError } from '@/domain/errors/unexpected-error'
+import { NotFoundError } from '@/domain/errors/not-found-error'
+import { ServerError } from '@/domain/errors/server-error'
 
 export class RemoteAuthentication {
   constructor (
@@ -16,8 +19,11 @@ export class RemoteAuthentication {
     })
 
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break
       case HttpStatusCode.unauthorized: throw new InvalidCrentialsError()
-      default: return Promise.resolve()
+      case HttpStatusCode.notFound: throw new NotFoundError()
+      case HttpStatusCode.serverError: throw new ServerError()
+      default: throw new UnexpectedError()
     }
   }
 }
