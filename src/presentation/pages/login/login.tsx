25e7ee281c12d217e6da10caa7ac572198808ade
Crisley Moura <crisley.alves@petcoach.co>
2020-10-09 08:54:24 -0300

    refactor: move input to a component

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Styles from './login-styles.scss';
 import Spinner from '@/presentation/components/spinner/spinner';
 import LoginHeader from '@/presentation/components/login-header/login-header';
+import Input from '@/presentation/components/input/input';
 import Footer from '@/presentation/components/footer/footer';
 
 const Login: React.FC = () => {
@@ -11,14 +12,9 @@ const Login: React.FC = () => {
       <LoginHeader />
       <form className={Styles.form}>
         <h2>Login</h2>
-        <div className={Styles.inputWrapper}>
-          <input type="email" name="email" placeholder="Enter your email" />
-          <span className={Styles.status}>k</span>
-        </div>
-        <div className={Styles.inputWrapper}>
-          <input type="password" name="password" placeholder="Enter your password" />
-          <span className={Styles.status}>k</span>
-        </div>
+        
+        <Input type="email" name="email" placeholder="Enter your email"/>
+        <Input type="password" name="password" placeholder="Enter your password"/>
         
         <button className={Styles.submit} type="submit">Enter</button>
         <span className={Styles.link}>create account</span>
