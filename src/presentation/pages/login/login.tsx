15d1c1f4a619aa4b39777d2e2425d83c90c6d044
Crisley Moura <crisley.alves@petcoach.co>
2020-10-19 14:11:20 -0300

    fix: fix space in login page

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -20,7 +20,7 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
   })
 
   useEffect(() => {
-    validation.validate('email', state.email )
+    validation.validate('email', state.email)
   }, [state.email])
 
   useEffect(() => {
